f3ecaef50876cbcb2cb638a4655c3f92939f03fd
Richard van Driel <richard.van.driel@hotmail.nl>
2018-03-24 20:39:03 +0100

    Release version 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+
+interface firestorecollectionPosts {
+  title: string;
+  content: string;
+}
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
-}
+
+  postsFirestorecollection: AngularFirestoreCollection<firestorecollectionPosts>;
+  postsObservable: Observable<firestorecollectionPosts[]>;
+
+  submitTitle: string;
+  submitContent: string;
+
+  constructor(private afs: AngularFirestore) {}
+
+  ngOnInit() {
+    this.postsFirestorecollection = this.afs.collection('fscPosts');
+    this.postsObservable = this.postsFirestorecollection.valueChanges();
+  }
+
+  addPost() {
+    this.afs.collection('fscPosts').add({'title': this.submitTitle, 'content': this.submitContent});
+  }
+} 
